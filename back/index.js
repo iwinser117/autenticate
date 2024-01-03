ea19ec8bbbcb910b2ba7ffcb43e22484b7ea4e2a
Iwinser Sanchez <iwinser.sanchez47@gmail.com>
2024-01-03 15:41:05 -0500

    ajuste new deploy

--- back/index.js
+++ back/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(cookieParser());
 const corsOptions = {
-  origin: "https://autenticate-bed32g5ts-iwinser117.vercel.app/",
+  origin: "https://autenticate.vercel.app/",
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
